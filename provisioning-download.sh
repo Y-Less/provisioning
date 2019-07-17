e8f7a7669e1d2a4da37fcac85b85538ebc0951b6
Y-Less <git@y-less.com>
2019-07-17 19:11:57 +0200

    Add files via upload

---
+++ provisioning-download.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-generic.sh
+chmod 777 provisioning-generic.sh
+
+wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-setup.sh
+chmod 777 provisioning-setup.sh
+
+wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-master.sh
+chmod 777 provisioning-master.sh
+
+#wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-client.sh
+#chmod 777 provisioning-client.sh
+
