d8b5876926b444509c44db0de07209eb547a9dc8
Y-Less <git@y-less.com>
2019-07-17 19:12:56 +0200

    Add files via upload

--- provisioning-download.sh
+++ provisioning-download.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-generic.sh
 chmod 777 provisioning-generic.sh
 
-wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-setup.sh
+wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-setup.sh > provisioning-setup.sh
 chmod 777 provisioning-setup.sh
 
-wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-master.sh
+wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-master.sh > provisioning-master.sh
 chmod 777 provisioning-master.sh
 
-#wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-client.sh
+#wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-client.sh > provisioning-client.sh
 #chmod 777 provisioning-client.sh
 
--- provisioning-main.sh
+++ provisioning-main.sh
@@ -1,4 +1,6 @@
-wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-main.sh > provisioning-download.sh
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/Y-Less/provisioning/master/provisioning-download.sh > provisioning-download.sh
 chmod 777 provisioning-download.sh
 ./provisioning-download.sh
 
